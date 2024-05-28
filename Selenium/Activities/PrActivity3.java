604d54be82a65534d25cea433f3862e493b41df3
Ashasirichandana <asha.sirichandana@gmail.com>
2024-05-28 11:21:57 +0530

    Add files via upload

---
+++ Selenium/Activities/PrActivity3.java
@@ -0,0 +1,35 @@
+package activities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class PrActivity3 {
+    public static void main(String[] args) {
+        // Set up Firefox driver
+        WebDriverManager.firefoxdriver().setup();
+        // Create a new instance of the Firefox driver
+        WebDriver driver = new FirefoxDriver();
+
+        // Open the page
+        driver.get("http://alchemy.hguy.co/orangehrm");
+        // Print the title of the page
+        System.out.println("Home page title: " + driver.getTitle());
+
+        // Find the username field and enter the username
+        driver.findElement(By.id("txtUsername")).sendKeys("orange");
+        // Find the password field and enter the password
+        driver.findElement(By.id("txtPassword")).sendKeys("orangepassword123");
+        // Find the login button and click it
+        driver.findElement(By.id("btnLogin")).click();
+
+        // Print the confirmation message
+        String message = driver.findElement(By.id("welcome")).getText();
+        System.out.println("Login message: " + message);
+
+        // Close the browser
+        driver.close();
+    }
+}
+
